20e723661f5035b456aab13f05772a740ce4bb3e
Marcelo G <m2biel000@icloud.com>
2024-04-29 17:39:46 -0300

    Using a theme with ThemeProvider

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export default {
+  COLORS: {
+    BG_LIGHT: "#262529",
+    BG_MEDIUM: "#1C1B1E",
+    BG_DARK: "#0D0C0F",
+    PINK: "#FF859B",
+    LABEL_GRAY: "#948F99",
+    FONT_WHITE: "#F4EDE8",
+    SUBTITLE_GRAY: "#CAC4CF",
+  },
+};
