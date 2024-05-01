1960883b397edc481d09f8400045e0b49ef8538e
Marcelo G <m2biel000@icloud.com>
2024-05-01 18:15:27 -0300

    Component folder and Button created/ imcs snippet to import {Container} from ./styles

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export function Button({ label, loading = false }) {
+  return (
+    <Container type="button" disabled={loading}>
+      {loading ? "Carregando..." : label}
+    </Container>
+  );
+}
