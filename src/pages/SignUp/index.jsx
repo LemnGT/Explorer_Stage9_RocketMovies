834f91b813ddb0bab1a876ed0060e50b0fe54f60
Marcelo G <m2biel000@icloud.com>
2024-06-28 16:05:14 -0300

    finished signUp page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,20 +2,30 @@ import { Container, Form, BG } from "./styles";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { ButtonText } from "../../components/ButtonText";
-import { IoIosLock, IoIosMail, IoIosUser } from "react-icons/io";
+import {
+  IoIosLock,
+  IoIosMail,
+  IoMdPerson,
+  IoIosArrowRoundBack,
+} from "react-icons/io";
 
-export function SignIn() {
+export function SignUp() {
   return (
     <Container>
       <Form>
         <h1>RocketMovies</h1>
         <p>Aplicação para acompanhar tudo que assistir.</p>
 
-        <h2>Faça seu Login</h2>
+        <h2>Crie sua conta</h2>
+
+        <Input type="text" placeholder="Nome" icon={IoMdPerson} />
         <Input type="text" placeholder="E-mail" icon={IoIosMail} />
         <Input type="password" placeholder="Senha" icon={IoIosLock} />
-        <Button label="Entrar" />
-        <ButtonText title="Criar conta" />
+        <Button label="Cadastrar" />
+        <ButtonText
+          title="Voltar para o login"
+          icon={<IoIosArrowRoundBack />}
+        />
       </Form>
       <BG />
     </Container>
